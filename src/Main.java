b83d011b664441649b9a6b03287ece4bdf9b7231
DavidAPinzon <dandrespinzon@ucundinamarca.edu.co>
2025-03-14 15:58:03 -0500

    encendedor2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 
 public class Main {
         public static void main(String[] args) {
-                // Crear tres objetos lógicos
+
                 Encendedor interruptorLuz = new Encendedor("Interruptor de luz", false);
                 Encendedor sensorMovimiento = new Encendedor("Sensor de movimiento", true);
                 Encendedor alarma = new Encendedor("Alarma", false);
 
-                // Interactuar con los objetos
                 interruptorLuz.encender();
                 sensorMovimiento.mostrarEstado();
                 alarma.encender();
 
-                // Apagar un objeto
+
                 alarma.apagar();
 
-                // Mostrar el estado final de los objetos
+
                 interruptorLuz.mostrarEstado();
                 sensorMovimiento.mostrarEstado();
                 alarma.mostrarEstado();
